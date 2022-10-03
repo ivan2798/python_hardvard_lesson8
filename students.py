e98a06521cdbdc9dcb0515c69722aba891f6d718
ivan2798 <inge1998@hotmail.com>
2022-10-02 23:25:31 -0500

    Lambda Functions

--- students.py
+++ students.py
@@ -3,10 +3,8 @@ students = []
 with open("students.csv") as file:
     for line in file:
         name, house = line.rstrip().split(",")
-        student = {}
-        student["name"] = name
-        student["house"] = house
+        student = {"name": name, "house": house}
         students.append(student)
 
-for student in students:
+for student in sorted(students, key=lambda student: student["name"]):
     print(f"{student['name']} is in {student['house']}")
